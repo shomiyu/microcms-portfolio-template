639b794faf8164833e5109659d3dda6ed744cfb7
shomiyu <m1011522@gmail.com>
2021-12-23 14:31:51 +0900

    初期構築

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <p>テスト</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped>
+// $test: #000;
+h1 {
+  @include mq() {
+    color: red;
+  }
+  font-size: fz(14);
+}
+</style>
